625c4eab69245713ed0660934c045d2e59182f74
rabdallah-99 <riham_m80@yahoo.com>
2022-02-12 02:06:19 +0000

    RA: started library schema

--- database/library.sql
+++ database/library.sql
@@ -3,6 +3,14 @@ USE library;
 CREATE TABLE books(
 book_id int NOT NULL AUTO_INCREMENT,
 book_name varchar(40) NOT NULL,
-
+publisher_id int NOT NULL,
 PRIMARY KEY(book_id)
+FOREIGN KEY (publisher_id) REFERENCES publishers(publisher_id)
 );
+
+CREATE TABLE publishers(
+publisher_id int NOT NULL AUTO_INCREMENT,
+publisher_name varchar(40) NOT NULL,
+publisher_email varchar (40),
+publisher_address varchar (60),
+PRIMARY KEY(publisher_id )
