2afd9e5b0b90524b14f6e94b8c9b6c7fead76e5a
rabdallah-99 <riham_m80@yahoo.com>
2022-02-12 14:48:26 +0000

    RA: adding to library schema

--- database/library.sql
+++ database/library.sql
@@ -1,10 +1,16 @@
 CREATE DATABASE IF NOT EXISTS library;
 USE library;
+
+CREATE TABLE category(
+category_id int NOT NULL AUTO,
+categoy_name varchar(40)
+):
 CREATE TABLE books(
 book_id int NOT NULL AUTO_INCREMENT,
 book_name varchar(40) NOT NULL,
 publisher_id int NOT NULL,
-PRIMARY KEY(book_id)
+category_id int NOT NULL,
+PRIMARY KEY(book_id),
 FOREIGN KEY (publisher_id) REFERENCES publishers(publisher_id)
 );
 
@@ -14,3 +20,4 @@ publisher_name varchar(40) NOT NULL,
 publisher_email varchar (40),
 publisher_address varchar (60),
 PRIMARY KEY(publisher_id )
+);
