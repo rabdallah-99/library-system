b667ed2134580f1c000db7a0f8a3203c260fafb7
rabdallah-99 <riham_m80@yahoo.com>
2022-02-24 23:13:38 +0000

    RA: modifying .gitignore

--- application/models.py
+++ application/models.py
@@ -16,13 +16,27 @@ class Borrower(db.Model):
     bdate = db.Column(Bdate)
     phone = db.Column(db.String(20))
 
-class login(db.Model):
+class Login(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     password = db.Column(db.String(50))
-   
-#class Books(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    name = db.Column(db.String(40))
-#    price = db.Column(db.Integer)
+
+   #need to write the relations between tables
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
+    author_id = db.Column(db.Integer)
+    category_id = db.Column(db.Integer)
+    price = db.Column(db.Integer)
+    count = db.Column(db.Integer)
+  #need to write the relations between tables
+class Transaction(db.Model):
+    transaction_id = db.Column(db.Integer, primary_key=True)
+    borrower_id = db.Column(db.Integer)
+    book_id = db.Column(db.Integer)
+    borrow_date = db.Column(db.Date)
+    return_date = db.Column(db.Date)
+    status = db.Column (db.String(5))
+
+
 
