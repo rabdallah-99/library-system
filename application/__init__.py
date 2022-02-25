03dddc8fd67408f6bf357da4364001df23943094
rabdallah-99 <riham_m80@yahoo.com>
2022-02-25 00:44:35 +0000

    RA: login form and user

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from os import getenv
+# Environmental variables need to be set at jenkins
 app = Flask(__name__)
+# Set login details from environment variables
+user = getenv('MYSQL_USER')
+password = getenv('MYSQL_PASSWORD')
+url = getenv('MYSQL_URL')
+db = getenv('MYSQL_DATABASE')
+secret = getenv('MYSQL_SECRETKEY')
 
-#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://riham:root@127.0.0.1:3306/library'
-#modify the config string with mysql connector
-#testing with hardcoded sting it should be removed in other versions with env
+# don't forget to switch
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://riham:root@127.0.0.1/library'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + user + ':' + password + '@' + url + '/' + db
+# Added security
+app.config['SECRET_KEY'] = secret
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from application import routes
--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,23 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
+from wtforms import StringField, DateTimeField, FloatField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from application import db, bcrypt
+from application.models import Borrower, Books, Author, Category, Transaction, Login
+#from flask_login import current_user
 # this file should build the available forms needed for project
+
+class LogInForm(FlaskForm):
+    name = StringField('User Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+    def validate_user(self, name, password):
+        user = Login.query.filter_by(name=name.data).first()
+
+        if user is None:
+            raise ValidationError('Unknown user name')
+
+        if bcrypt.check_password_hash(user.password, password.data):
+            raise ValidationError('Incorrect password')
+
