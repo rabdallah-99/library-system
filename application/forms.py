a7c5f97dfee49d5295720617d2344a2b69d5558b
rabdallah-99 <riham_m80@yahoo.com>
2022-02-28 02:00:27 +0000

    RA: adding borrower, book not yet

--- application/forms.py
+++ application/forms.py
@@ -2,16 +2,19 @@ from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, FloatField, PasswordField, BooleanField, SubmitField, SelectField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+#from wtforms.fields.html5 import DateField
 from application import db, bcrypt, conn
 from application.models import Borrower, Books, Author, Category, Transaction, Login
 #from flask_login import current_user
 # this file should build the available forms needed for project
+
 
 class LogInForm(FlaskForm):
     name = StringField('User Name', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
     def validate_user(self, name, password):
         user = Login.query.filter_by(name=name.data).first()
 
@@ -22,39 +25,42 @@ class LogInForm(FlaskForm):
             raise ValidationError('Incorrect password')
 
 # Class for signing up
+
+
 class SignUpForm(FlaskForm):
-    user = StringField('User Name',validators = [DataRequired(), Length(min=3, max=15)])
+    user = StringField('User Name', validators=[DataRequired(), Length(min=3, max=15)])
 
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-class AddCategory(FlaskForm) :
+
+class AddCategory(FlaskForm):
     name = StringField('Category Name', validators=[DataRequired()])
     submit = SubmitField('Add Category')
 
-class AddAuthor(FlaskForm) :
-    name = StringField('Author Name',validators=[DataRequired()])
+
+class AddAuthor(FlaskForm):
+    name = StringField('Author Name', validators=[DataRequired()])
     submit = SubmitField('Add Author')
 
+
 class AddBorrower(FlaskForm):
-    name = StringField('Borrower Name',validators=[DataRequired()])
-    address = StringField('Borrower Address',validators=[DataRequired()])
-    bdate = DateField('Birthday date')
-    phone = StringField('Borrower phone')
+    borrower_name = StringField('Borrower Name', validators=[DataRequired()])
+    borrower_address = StringField('Borrower Address', validators=[DataRequired()])
+    borrower_bdate = DateField('Birthday date',format='%Y-%m-%d')
+    borrower_phone = StringField('Borrower phone')
     submit = SubmitField('Add Borrower')
+
 
 class AddBook(FlaskForm):
 
-    name = FloatField('Book Name',validators=[DataRequired()])
-    author_id=SelectField('Author', coerce=int)
-    category_id=SelectField('Category', coerce=int)
+    book_name = StringField('Book Name', validators=[DataRequired()])
+    author_id = SelectField('Author', coerce=int)
+    category_id = SelectField('Category', coerce=int)
     price = FloatField('Book Price')
-    count = IntegerField ('Book Count')
-    available=BooleanField('Available')
-
-
+    count = IntegerField('Book Count')
+    available = BooleanField('Available')
+    submit = SubmitField('Add Book')
 #class DeleteCategory(FlaskForm) :
-
-
 
--- application/routes.py
+++ application/routes.py
@@ -3,39 +3,60 @@ from application import app, db, conn
 from application.forms import AddCategory, AddAuthor, LogInForm, SignUpForm, AddBook, AddBorrower
 from application.models import Category, Author, Borrower, Login, Books
 #modify this file to suit your database and site layout
+
+
 @app.route('/category', methods=['GET', 'POST'])
 def add_category():
-    form=AddCategory()
+    form = AddCategory()
     if form.validate_on_submit():
-        new_category = Category (category_id=0, category_name = form.name.data)
+        new_category = Category(category_id=0, category_name=form.name.data)
         db.session.add(new_category)
         db.session.commit()
-    return render_template('Category.html', title='Category', form = form)
+    return render_template('Category.html', title='Category', form=form)
 #@app.route('/deletecategory',methods=['GET','POST'])
 #def delete_category():
- #   form=DeleteCategory()
+#   form=DeleteCategory()
+
 
 @app.route('/author', methods=['GET', 'POST'])
 def add_author():
-    form=AddAuthor()
+    form = AddAuthor()
     if form.validate_on_submit():
         new_author = Author(author_id=0, author_name=form.name.data)
         db.session.add(new_author)
         db.session.commit()
-    return render_template('author.html', title='Author', form = form)
-@app.route('/book')
+    return render_template('author.html', title='Author', form=form)
+
+
+@app.route('/book', methods=['GET', 'POST'])
 def addbook():
-    form=AddBook()
+    form = AddBook()
     cursor = conn.cursor()
     cursor.execute('SELECT author_id,author_name FROM author')
     joblist = cursor.fetchall()
+    form.author_id.choices = [(h[0], h[1]) for h in joblist]
     cursor.execute('SELECT category_id,category_name FROM category')
     joblist1 = cursor.fetchall()
+    form.category_id.choices = [(h[0], h[1]) for h in joblist1]
     if form.validate_on_submit():
-        new_book=Books(book_id=0,book_name=form.name.data,author_id=form.author_id.data,category_id=form.category_id.data, price=form.price.data,count=form.count.data, available=form.available.data)
+        #return '<html><h1> {} </h1></html>'.format(form.author_id.data)
+        id=db.Query('SELECT author_id FROM author WHERE author_name ='+form.author_id.data)
+        category=db.Query('SELECT category_id FROM category WHERE category_name='+form.category_id.data)
+        new_book = Books(book_id=0, book_name=form.book_name.data, author_id=id, category_id=category, price=form.price.data, count=form.count.data)
         db.session.add(new_book)
         db.session.commit()
-    return render_template("book.html",joblist=joblist, joblist1=joblist1 )
+
+    return render_template("book.html", joblist=joblist, joblist1=joblist1, form=form)
+
+@app.route('/borrower', methods=['GET', 'POST'])
+def addBorrower():
+    form = AddBorrower()
+    if form.validate_on_submit():
+        new_borrower = Borrower(borrower_id=0, borrower_name=form.borrower_name.data, borrower_address=form.borrower_address.data, borrower_bdate=form.borrower_bdate.data, borrower_phone=form.borrower_phone.data)
+        db.session.add(new_borrower)
+        db.session.commit()
+    return render_template('borrower.html', title='borrower', form=form)
+
 
 @app.route('/read')
 def readcategory():
@@ -43,24 +64,31 @@ def readcategory():
     all_category = Category.query.all()
     category_string = ""
     for category in all_category:
-        category_string += "<br>"+ category.category_name
+        category_string += "<br>" + category.category_name
     return category_string
+
+
 @app.route('/read-author')
 def readauthor():
 
     all_authors = Author.query.all()
     author_string = ""
     for author in all_authors:
-        author_string += "<br>"+ author.author_name
-    return  author_string
+        author_string += "<br>"\
+                         + author.author_name
+    return author_string
 # Route to home page
+
+
 @app.route('/')
 @app.route('/home')
 def home():
 
-    return render_template('home.html', title = 'Home')
+    return render_template('home.html', title='Home')
 
 # Route to about page
+
+
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
