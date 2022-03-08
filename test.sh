e0178c9a3051e88348881cf92f982762b5b74022
rabdallah-99 <riham_m80@yahoo.com>
2022-03-08 10:21:30 +0000

    RA: removing venv activate

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #always activate virtual environment
-source venv/bin/activate
 #run pytest
 python3 -m pytest --cov=application --cov-report term-missing --cov-report xml:coverage.xml --junitxml=junit_report.xml
