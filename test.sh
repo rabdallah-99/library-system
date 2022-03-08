2bcffe0fdae2cd3f786799e73f8fa03dfbfae593
rabdallah-99 <riham_m80@yahoo.com>
2022-03-08 10:27:30 +0000

    RA: reinstalling requiremnts

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #always activate virtual environment
 #run pytest
+pip3 install -r requirements.txt
 python3 -m pytest --cov=application --cov-report term-missing --cov-report xml:coverage.xml --junitxml=junit_report.xml
