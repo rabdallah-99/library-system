a2d7e9ec9c51ceb6355599a3dad809c6d410ef8e
rabdallah-99 <riham_m80@yahoo.com>
2022-03-11 01:03:38 +0000

    RA:docker update

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM python:3.6
 #WORKDIR /app
 COPY . .
 RUN python -m pip install --upgrade pip
-RUN pip install -r requirements.txt
+RUN apt install python3-venv
+RUN python3 -m venv venv && source venv/bin/activate && pip install -r requirements.txt
 EXPOSE 5000
 
 ENTRYPOINT ["python","app.py"]
--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -9,12 +9,12 @@ docker build -t flask-library:latest .
 docker run -d --name mysql --network mynetwork databasemysql:8
 
 #run flask container
-docker run -d --name flask-app --network mynetwork flask-library:latest 
+docker run -d --name flask-app --network mynetwork flask-library:latest
 
 #run nginx container
 docker run -d --name nginx -p 80:80 --network mynetwork --mount type=bind,source=$(pwd)/nginx.conf,target=/etc/nginx/nginx.conf nginx:latest
 
-show running containers
+#show running containers
 
 
 docker ps
